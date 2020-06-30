b3ab13fa3a3bce9cbd3d2d76c519afe019893279
James Pegg <james.l.pegg@gmail.com>
2020-06-30 14:51:50 -0700

    Completed dynamic menu v1

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
  * 
+ * Added: collapsible/expandible nav
+ * Added: sections collapsible
+ * Added: hide nav when scrolling
+ * Added: scroll to top button
+ * 
  * Dependencies: None
  * 
  * JS Version: ES2015/ES6
@@ -34,12 +39,37 @@
 */
 
 // build the nav
-
-
 // Add class 'active' to section when near top of viewport
-
-
 // Scroll to anchor ID using scrollTO event
+
+/**
+ * Nav bar will display text "Portfolio Sections" - this is the container
+ * Nav Sections in the portfolio will be dynamically generated
+ * based on loaded content
+ * This will be collabsible/expandible
+ */
+    // const navSection = document.querySelector('H2').textContent;
+
+
+function genNavBar () {
+    menuCreation();
+
+    function menuCreation() {
+        let head = document.querySelectorAll('h2');
+        // let link = "section";
+        for (let i = 0; i < head.length; i++) {
+            const newNav = document.createElement('li');
+            let z = i + 1;
+            newNav.innerHTML = '<a class = "menu__link" href = "#section' + z + '">' + head[i].textContent + '</a>';
+            const newList = document.querySelector('#navbar__list');
+            newList.appendChild(newNav);
+        }
+    }
+}
+
+genNavBar();
+
+
 
 
 /**
